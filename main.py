0ddd1d4cc02a576f329a27793c13a8bf1ffe9b7a
madeyexz <ian.xiao@stu.pku.edu.cn>
2023-11-26 16:11:16 +0800

    updated

--- main.py
+++ main.py
@@ -10,23 +10,21 @@ def main():
     # for each entry in the list, create a person object and upsert the vector into pinecone index
     
     '''ADDING USER INFORMATION'''
-    usr_data = [["lara",18,False,True,"BJ","A@gmail.com","I am a stone"],["lance",19,True,True,"BJ","B@gmail.com","I am a cat"],["jimmy",25,True,True,"BJ","C@gmail.com","I am a kitten"]]
+    preset_data = [["lara",18,False,True,"BJ","A@gmail.com","I am a stone"],["lance",19,True,True,"BJ","B@gmail.com","I am a cat"],["jimmy",25,True,True,"BJ","C@gmail.com","I am a kitten"]]
     
     ppl = []
-    for i in usr_data:
-        ppl.append(Person(i[0],i[1],i[2],i[3],i[4],i[5],i[6]))
+    for i in preset_data:
+        try:
+            ppl.append(Person(i[0],i[1],i[2],i[3],i[4],i[5],i[6]))
+        except ValueError as e:
+            print(f"Error: {e}")
+            '''API COMMUNICATIONS HERE'''
+            
+    '''usr input'''
+    '''for each new user input, create a new Person object, add it into ppl[]'''
     
     with open("data.txt", "w") as f:
         f.write(str(pinecone_fetch(ppl[0], pinecone_index)))
-    
-    # ppl = [Person(A), Person(B), Person(C)]
-    
-    # will result in
-    # ppl = [
-    #     "lara@gmail":Person("lara",18,False,True,"BJ","@gmail.com","I am a capybara"),
-    #     "lance@gmail":Person("lance",19,True,True,"BJ","@gmail.com","I am a cat"),
-    #     "jimmy@gmail.com":Person("jimmy",25,True,True,"BJ","@gmail.com","I am a dude"),
-    # ]
     
     for i in ppl:
         pinecone_vector_upsert(i, pinecone_index)
@@ -34,19 +32,21 @@ def main():
     print(pinecone_index.describe_index_stats())
     
     '''QUERYING'''
-    qperson = ppl[-1]
-    qresult = pinecone_query(pinecone_index, qperson, 1)
+    qperson = ppl[0]
+    qresult = pinecone_query(pinecone_index, qperson, 3)
     
     '''PRINTING RESULTS'''
     print(qresult)
     print()
     
     '''FINDING INFORMATION OF MATCHES'''
-    
-    
 
     for j in qresult:
-        if j == qresult[0]:
+        best_match = qresult[1]
+        himself = qresult[0]
+        if j==himself:
+            continue
+        elif j == best_match:
             print("This is your best match!")
         name, email = j[0].split("#")
         score = j[1]
