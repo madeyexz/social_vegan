461d6e6cb6e23117b7db31dfd58607a22bbbba01
Ian Hsiao <ian.xiao@stu.pku.edu.cn>
2023-11-30 11:49:11 +0800

    Delete fastapi.py

--- fastapi.py
+++
@@ -1,9 +0,0 @@
-# main.py
-from typing import List
-from uuid import uuid4, UUID
-from fastapi import FastAPI, HTTPException
-# from models import Gender, Role, User, UpdateUser
-from person_class import Person
-
-app = FastAPI()
-
